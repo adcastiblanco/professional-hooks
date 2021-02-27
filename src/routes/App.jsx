79a2c9e22a46af94674905f4b35525227c362b60
Angel Castiblanco <angelcastiblanco@Angels-MacBook-Pro.local>
2021-02-27 16:20:54 -0500

    Added useContext

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -4,20 +4,26 @@ import Layout from '../components/Layout'
 
 import {Home, Checkout, Information, Payment, Success, NotFound} from '../containers'
 
+import AppContext from '../context/AppContext'
+import useInitialState from '../hooks/UseInitialState'
+
 function App() {
+    const initialState = useInitialState()
     return (
-        <BrowserRouter>
-            <Layout>
-                <Switch>
-                    <Route exact path="/" component={Home}/>
-                    <Route exact path="/checkout" component={Checkout}/>
-                    <Route exact path="/checkout/information" component={Information}/>
-                    <Route exact path="/checkout/payment" component={Payment}/>
-                    <Route exact path="/checkout/success" component={Success}/>
-                    <Route component={NotFound}/>
-                </Switch>
-            </Layout>
-        </BrowserRouter>
+        <AppContext.Provider value={initialState}>
+            <BrowserRouter>
+                <Layout>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route exact path="/checkout" component={Checkout}/>
+                        <Route exact path="/checkout/information" component={Information}/>
+                        <Route exact path="/checkout/payment" component={Payment}/>
+                        <Route exact path="/checkout/success" component={Success}/>
+                        <Route component={NotFound}/>
+                    </Switch>
+                </Layout>
+            </BrowserRouter>
+        </AppContext.Provider>
     )
 }
 
